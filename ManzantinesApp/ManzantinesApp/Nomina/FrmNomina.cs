1137ca946cc387449ef4861b1a5cdc649a9abbc1
Victor Véliz <ing-vj@outlook.com>
2019-05-20 21:09:27 -0400

    Agregado frm nomina e inicio de actualizacion para nomina

---
+++ ManzantinesApp/ManzantinesApp/Nomina/FrmNomina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManzantinesApp.Nomina
+{
+    public partial class FrmNomina : Form
+    {
+        public FrmNomina()
+        {
+            InitializeComponent();
+        }
+    }
+}
