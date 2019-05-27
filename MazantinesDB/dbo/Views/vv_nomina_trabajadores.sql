a040da07b999b84e3059604d4e947be9902ea34f
Victor Véliz <vveliz@bufalinda.com>
2019-05-27 00:14:29 -0400

    Updated vista vv_nomina_trabajadores

--- MazantinesDB/dbo/Views/vv_nomina_trabajadores.sql
+++ MazantinesDB/dbo/Views/vv_nomina_trabajadores.sql
@@ -1,10 +1,13 @@
-﻿CREATE VIEW [dbo].[vv_nomina_trabajadores] AS SELECT 
+﻿CREATE VIEW [dbo].[vv_nomina_trabajadores] AS SELECT
 	tra.id,
 	tra.Nro_empleado,
-	CONCAT(tra.Nombre,' ', tra.Apellidos) as trabajador,
-	fin.id_encargado
+	CONCAT ( tra.Nombre, ' ', tra.Apellidos ) AS trabajador,
+	fin.id_encargado,
+	( SELECT TOP 1 id_empleo FROM Trabajadores_Empleos WHERE Trabajadores_Empleos.id_trabajador = tra.id ) AS id_empleo,
+	ISNULL( pag.importeDia, 0 ) AS valorD,
+	ISNULL( pag.importeHoras, 0 ) AS valorH 
 FROM
-	Trabajadores as tra
-LEFT JOIN Casas as casa ON casa.Id = tra.id_casa
-LEFT JOIN Fincas as fin ON fin.Id = casa.id_finca
---LEFT JOIN Encargados as enc ON enc.Id = fin.id_encargado
+	Trabajadores AS tra
+	LEFT JOIN Casas AS casa ON casa.Id = tra.id_casa
+	LEFT JOIN Fincas AS fin ON fin.Id = casa.id_finca
+	LEFT JOIN EmpleosPagos AS pag ON pag.id_empleo = id_empleo --LEFT JOIN Encargados as enc ON enc.Id = fin.id_encargado
