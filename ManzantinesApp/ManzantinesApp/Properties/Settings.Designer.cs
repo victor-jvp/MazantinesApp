fbcb8398689aaf142cd9726f381263b8e73ce741
Victor Veliz <ing.vj15@gmail.com>
2019-12-04 14:50:38 -0400

    Configuracion adaptada para utilizar en red

--- ManzantinesApp/ManzantinesApp/Properties/Settings.Designer.cs
+++ ManzantinesApp/ManzantinesApp/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ManzantinesApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,7 +44,8 @@ namespace ManzantinesApp.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=Mazantines;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PRINCIPAL;Initial Catalog=Mazantines;Persist Security Info=True;User " +
+            "ID=sa;Password=Solomon2011")]
         public string MazantinesConnectionString {
             get {
                 return ((string)(this["MazantinesConnectionString"]));
